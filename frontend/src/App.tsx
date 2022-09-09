d22e306b8096fed4f1a3c204345e4d758519c5d4
allankevinscain <meuemail44allan@gmail.com>
2022-09-09 13:47:27 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/header";
 import { SalesCard } from "./components/salesCard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/notifyButton/index.tsx
+++ frontend/src/components/notifyButton/index.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import IconUser from "../../assets/img/user.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
 export const NotifyButton: React.FC<{ saleId: number }> = ({ saleId }) => {
   const handleClick = (id: number) => {
-    axios(`${BASE_URL}/sales/${id}/notification`).then((res) => {});
+    axios(`${BASE_URL}/sales/${id}/notification`).then((res) => {
+      toast.info("SMS envido com sucesso!");
+    });
   };
 
   return (
