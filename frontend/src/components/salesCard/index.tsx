fc6430dc2cfafcf102c748492c47e68287b12f93
allankevinscain <meuemail44allan@gmail.com>
2022-09-09 13:34:28 -0300

    date update

--- frontend/src/components/salesCard/index.tsx
+++ frontend/src/components/salesCard/index.tsx
@@ -16,8 +16,13 @@ export const SalesCard: React.FC = () => {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((res) => setSales(res.data.content));
-  }, []);
+    const dateMin = minDate.toISOString().slice(0, 10);
+    const dateMax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dateMin}&maxDate=${dateMax}`)
+      .then((res) => setSales(res.data.content));
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
